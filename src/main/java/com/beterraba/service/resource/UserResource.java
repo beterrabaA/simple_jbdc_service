a3c384f48666857983caf66db23011888bbe5756
Beterraba <ls6182315@gmail.com>
2024-09-16 18:00:07 -0300

    h2 database and jpa impl

--- src/main/java/com/beterraba/service/resource/UserResource.java
+++ src/main/java/com/beterraba/service/resource/UserResource.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.beterraba.service.entities.User;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@GetMapping
 	public ResponseEntity<User> findAll() {
-		User user = new User(1L,"Maria","maria@gmail.com","88995663645","senha123");
+		User user = new User(1L, "Maria", "maria@gmail.com", "88995663645", "senha123");
 		return ResponseEntity.ok().body(user);
 	}
 }
