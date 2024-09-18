fbc54e84a708dd4a5af88c69ddcc20fb5b2e0223
Beterraba <ls6182315@gmail.com>
2024-09-18 18:18:05 -0300

    user service layer

--- src/main/java/com/beterraba/service/resource/UserResource.java
+++ src/main/java/com/beterraba/service/resource/UserResource.java
@@ -1,19 +1,33 @@
 package com.beterraba.service.resource;
 
+import com.beterraba.service.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beterraba.service.entities.User;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Maria", "maria@gmail.com", "88995663645", "senha123");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> userList = service.findAll();
+		return ResponseEntity.ok().body(userList);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 }
