8099a723a1d607e3af3b082a373387eb4e3753e3
Beterraba <ls6182315@gmail.com>
2024-09-19 15:46:18 -0300

    many-to-many category&product

--- src/main/java/com/beterraba/service/config/TestConfig.java
+++ src/main/java/com/beterraba/service/config/TestConfig.java
@@ -46,6 +46,9 @@ public class TestConfig  implements CommandLineRunner {
         Product p4 = new Product("PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product ("Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User u1 = new User("Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User("Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -62,7 +65,6 @@ public class TestConfig  implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
-        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
     }
 }
--- src/main/java/com/beterraba/service/entities/Category.java
+++ src/main/java/com/beterraba/service/entities/Category.java
@@ -1,5 +1,6 @@
 package com.beterraba.service.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -20,7 +21,8 @@ public class Category implements Serializable {
     private long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<Product>();
 
     public Category() {}
