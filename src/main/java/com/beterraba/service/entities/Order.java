6c0c517ab1ae105609f4d76e9f9de62b9b0beac2
Beterraba <ls6182315@gmail.com>
2024-09-20 17:44:30 -0300

    payment entity

--- src/main/java/com/beterraba/service/entities/Order.java
+++ src/main/java/com/beterraba/service/entities/Order.java
@@ -34,6 +34,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<OrderItem>();
 
+    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {};
 
     public Order(Instant moment,OrderStatus status, User client) {
@@ -72,6 +75,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
